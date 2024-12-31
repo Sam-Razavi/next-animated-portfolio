6d38ac12f4c23d3d5e223b68bbb5946a77b929e8
Sam-Razavi <sam.sahbaei.razavi@iths.se>
2024-12-31 19:09:32 +0100

    added some expirences

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -295,111 +295,124 @@ const AboutPage = () => {
                             EXPERIENCE
                         </motion.h1>
                         {/* EXPERIENCE LIST */}
+                    </div>
+                    <div
+                        className="flex flex-col gap-12 justify-center pb-48"
+                        ref={experienceRef}
+                    >
+                        <motion.h1
+                            initial={{ x: '-300px' }}
+                            animate={isExperienceRefInView ? { x: '0' } : {}}
+                            transition={{ delay: 0.2 }}
+                            className="font-bold text-2xl"
+                        >
+                            EXPERIENCE
+                        </motion.h1>
                         <motion.div
                             initial={{ x: '-300px' }}
                             animate={isExperienceRefInView ? { x: '0' } : {}}
                             className=""
                         >
-                            {/* EXPERIENCE LIST ITEM */}
                             <div className="flex justify-between h-48">
-                                {/* LEFT */}
                                 <div className="w-1/3 ">
-                                    {/* JOB TITLE */}
                                     <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
-                                        Senior JavaScript Engineer
+                                        Internship
                                     </div>
-                                    {/* JOB DESC */}
                                     <div className="p-3 text-sm italic">
-                                        I led web development, offering
-                                        expertise in JavaScript frameworks.{' '}
+                                        Contributed to a project enhancing recruiter-consultant
+                                        connections using JavaScript and Angular, alongside
+                                        Bootstrap, SCSS, AWS, Git/GitHub, TypeScript, and
+                                        PostgreSQL.
                                     </div>
-                                    {/* JOB DATE */}
                                     <div className="p-3 text-red-400 text-sm font-semibold">
-                                        2024 - Present
+                                        2024
                                     </div>
-                                    {/* JOB COMPANY */}
                                     <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
-                                        Apple
+                                        Fill Technology
                                     </div>
                                 </div>
-                                {/* CENTER */}
                                 <div className="w-1/6 flex justify-center">
-                                    {/* LINE */}
                                     <div className="w-1 h-full bg-gray-600 rounded relative">
-                                        {/* LINE CIRCLE */}
                                         <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
                                     </div>
                                 </div>
-                                {/* RIGHT */}
                                 <div className="w-1/3 "></div>
                             </div>
-                            {/* EXPERIENCE LIST ITEM */}
                             <div className="flex justify-between h-48">
-                                {/* LEFT */}
                                 <div className="w-1/3 "></div>
-                                {/* CENTER */}
                                 <div className="w-1/6 flex justify-center">
-                                    {/* LINE */}
                                     <div className="w-1 h-full bg-gray-600 rounded relative">
-                                        {/* LINE CIRCLE */}
                                         <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
                                     </div>
                                 </div>
-                                {/* RIGHT */}
                                 <div className="w-1/3 ">
-                                    {/* JOB TITLE */}
                                     <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
-                                        Senior React Developer
+                                        Internship
                                     </div>
-                                    {/* JOB DESC */}
                                     <div className="p-3 text-sm italic">
-                                        I spearheaded React-based application
-                                        development, leveraging advanced skills.{' '}
+                                        Supported frontend development for a Smart RoomHub’s
+                                        platform, aligning with Devward's aim to improve energy
+                                        efficiency and indoor air quality through AI.
                                     </div>
-                                    {/* JOB DATE */}
                                     <div className="p-3 text-red-400 text-sm font-semibold">
-                                        2019 - 2024{' '}
+                                        2023
                                     </div>
-                                    {/* JOB COMPANY */}
                                     <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
-                                        Microsoft
+                                        Devward
                                     </div>
                                 </div>
                             </div>
-                            {/* EXPERIENCE LIST ITEM */}
                             <div className="flex justify-between h-48">
-                                {/* LEFT */}
                                 <div className="w-1/3 ">
-                                    {/* JOB TITLE */}
                                     <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
-                                        Freelancer{' '}
+                                        IT-Support
                                     </div>
-                                    {/* JOB DESC */}
                                     <div className="p-3 text-sm italic">
-                                        I provided web solutions, applying a
-                                        range of technologies to address client
-                                        requirements.{' '}
+                                        Troubleshoot and find solutions to technical problems
+                                        connected to IT.
                                     </div>
-                                    {/* JOB DATE */}
                                     <div className="p-3 text-red-400 text-sm font-semibold">
-                                        2010 - 2019{' '}
+                                        2017 - 2020
+                                    </div>
+                                    <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                                        OM ALNOOR AB
                                     </div>
                                 </div>
-                                {/* CENTER */}
                                 <div className="w-1/6 flex justify-center">
-                                    {/* LINE */}
                                     <div className="w-1 h-full bg-gray-600 rounded relative">
-                                        {/* LINE CIRCLE */}
                                         <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
                                     </div>
                                 </div>
-                                {/* RIGHT */}
                                 <div className="w-1/3 "></div>
+                            </div>
+                            <div className="flex justify-between h-48">
+                                <div className="w-1/3 ">
+                                    <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                                        IT-Support
+                                    </div>
+                                    <div className="p-3 text-sm italic">
+                                        Assisted users in resolving IT-related issues, ensuring
+                                        optimal system performance and user satisfaction.
+                                    </div>
+                                    <div className="p-3 text-red-400 text-sm font-semibold">
+                                        2015 - 2017
+                                    </div>
+                                    <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                                        XYZ Solutions AB
+                                    </div>
+                                </div>
+                                <div className="w-1/6 flex justify-center">
+                                    <div className="w-1 h-full bg-gray-600 rounded relative">
+                                        <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                                    </div>
+
+                                </div>
+                                <div className="w-1/3 ">
+                                </div>
                             </div>
                         </motion.div>
                     </div>
                 </div>
-                {/* SVG CONTAINER */}
                 <div className="hidden lg:block w-1/3 sticky top-0 z-30 xl:w-1/2">
                     <Brain scrollYProgress={scrollYProgress} />
                 </div>
